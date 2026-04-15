daa101c046438f89011b782a793f77ced263773f
parthuurandur199-cpu <parthuurandur199@gmail.com>
2026-04-16 00:03:50 +0530

    Update build.gradle.kts

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -2,5 +2,5 @@
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.1.21" apply false
-    id("com.google.devtools.ksp") version "2.1.21-2.0.0" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.2" apply false
 }
